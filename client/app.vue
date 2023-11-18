bddc972a372f5af7f85180a0c7f74acf3ca602d2
tolya <tolya.trick@bk.ru>
2023-11-18 21:05:39 +0700

    перенес данные в новый репозиторий

--- client/app.vue
+++ client/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div class="page">
+        <BaseHeader />
+        <NuxtPage />
+        <BaseFooter />
+    </div>
 </template>
---
+++ client/components/base/footer/footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>footer</div>
+</template>
+
+<script setup lang="typescript">
+
+</script>
